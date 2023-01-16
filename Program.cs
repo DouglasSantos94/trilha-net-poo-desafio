26aba4cea29c05cc0eafcea86b123f3b972581d2
DouglasSantos94 <dgstos94@gmail.com>
2023-01-16 19:45:30 -0300

    feat: create inheritance and tests

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+
+Smartphone nokia = new Nokia(numero: "5555555555", modelo: "2310", imei: "5489419648", memoria: 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Waze");
+
+Console.WriteLine("iPhone: ");
+
+Smartphone iphone = new Iphone(numero: "666666", modelo: "XS", imei: "1854189126", memoria: 256);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("Waze");
